f1b0d9e7a27839c077ebb9d44d131d200de55b52
nbndung <dung.nguyenbangoc@gmail.com>
2026-02-10 15:17:56 +0700

    - add check box - make condition if check box is checked and submit button is clicked then balloons - add md file - move progress bar inside condition - change progress bar to float

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ st.write("hello", 10)
 st.write(":grin:")
 st.write(5)
 
-my_bar = st.progress(10, "wow")
-for per in range(90):
-    time.sleep(0.01)
-    my_bar.progress(per + 10)
+
 
 check_box = st.checkbox("I confirm")
 button = st.button("Submit")
 if check_box and button:
+    my_bar = st.progress(0.0, "wow")
+    for per in range(10):
+        time.sleep(0.05)
+        my_bar.progress(per/10)
     st.balloons()
 
