84f935fef05414afb4a66bac8f3ac440155c2c60
nbndung <dung.nguyenbangoc@gmail.com>
2026-02-25 17:35:28 +0700

    - using streamlit icon

--- lesson_6_github_and_SCC.md
+++ lesson_6_github_and_SCC.md
@@ -1,2 +1,7 @@
+CONNECT TO GITHUB
 - first create an account on github
 - login streamlit using that account
+
+DELETE CHANGES REMOTE
+- git reset --hard <commit-hash-before-the-change>
+- git push origin <branch-name> --force
--- main.py
+++ main.py
@@ -1,17 +1,54 @@
 """
 - basic create and connect streamlit with github: https://www.youtube.com/watch?v=IVhskhyjXNY
 - connect pycharm project to github: https://www.youtube.com/watch?v=cOBv4HFfS-M&t=107s
+
+- streamlit icons: https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 """
 
 import streamlit as st
 import time
 
+# https://docs.streamlit.io/develop/api-reference/configuration/st.set_page_config
+st.set_page_config(
+    page_title="Cool App",
+    # page_icon="😊",           # can either add an icon
+    # page_icon=":joy:"
+    page_icon="smile.png",      # or an image
+    layout="centered",              # align everything to "centered" (default), or "wide" to fit the web's width
+    initial_sidebar_state="expanded" # setting the behaviour of sidebar (st.sidebar) if exists
+)
+
 st.title("Streamlit Test")
 
-st.write("hello", 10)
-st.write(":grin:")
-st.write(5)
+st.sidebar.title("Sidebar")
 
+with st.sidebar:
+    st.write("1. Item 1")
+    st.write("2. Item 2")
+
+with st.expander("Hidden text"):
+    st.write("hello", 10)
+    st.write(":grin:")
+    st.write(5)
+
+# https://docs.streamlit.io/develop/api-reference/layout/st.columns
+# https://www.youtube.com/watch?v=kTZg48MQ-t0
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("JavaScript")
+    st.image("javascript.png")
+    st.link_button("JavaScript", "https://www.javascript.com/")
+
+with col2:
+    st.header("Python")
+    st.image("python.png")
+    st.link_button("Python", "https://www.python.org/")
+
+with col3:
+    st.header("Java")
+    st.image("java.png")
+    st.link_button("Java", "https://www.java.com/en/")
 
 
 check_box = st.checkbox("I confirm")
